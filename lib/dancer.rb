e20829b232c92dfd34e3349ff454d3c9800b3007
Evan Pavone <evanrpavone@gmail.com>
2020-08-11 19:36:49 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,4 +4,10 @@ require_relative './fancy_dance.rb'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  extend MetaDancing
+  include Dance
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
 end
